213b63a3a0157cd9012a506ea5ea34d3bfa164be
siaheguelable <siaheguelable>
2025-03-13 10:06:53 -0700

    commit mobile

---
+++ WEEK02/scripts/bom.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('');
+const button = document.querySelector('button');
+const list = document.querySelector('');
+
+button.addEventListener('click', () => {
+    if (input.value != '') { };
+    else:
+});
